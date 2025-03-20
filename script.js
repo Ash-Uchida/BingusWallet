62fb304e23ee75ac991d5019f531c3f4893f11ac
Ash-Uchida <ash.uchida0w0@gmail.com>
2025-03-20 14:47:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCEuZH9Ctva_A2XsZjbhyy2We8XwIpabo8",
+    authDomain: "bingus-wallet.firebaseapp.com",
+    projectId: "bingus-wallet",
+    storageBucket: "bingus-wallet.firebasestorage.app",
+    messagingSenderId: "183867183994",
+    appId: "1:183867183994:web:ce75da09e4cd0195d17cc3",
+    measurementId: "G-GTL7RDRW4T"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+// Authentication Handling
+function signUp() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    auth.createUserWithEmailAndPassword(email, password)
+        .then(() => alert("User signed up!"))
+        .catch(error => alert(error.message));
+}
+
+function signIn() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    auth.signInWithEmailAndPassword(email, password)
+        .then(() => alert("User signed in!"))
+        .catch(error => alert(error.message));
+}
+
+function signOut() {
+    auth.signOut()
+        .then(() => alert("User signed out!"))
+        .catch(error => alert(error.message));
+}
+
 let balance = 0;
 let monthlyBudget = 0;
 
@@ -25,7 +64,7 @@ function addTransaction() {
 
     const transactionList = document.getElementById('transactions');
     const listItem = document.createElement('li');
-    listItem.textContent = ${description}: $${amount} (${type});
+    listItem.textContent = `${description}: $${amount} (${type})`;
     
     transactionList.appendChild(listItem);
     
